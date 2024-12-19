6ec46311711dd3cd76312740f2e5122b9235f0d8
julianmrios <julianmrios@gmail.com>
2024-12-19 13:38:10 -0300

    1.0

--- createapache.sh
+++ createapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y apache2
-echo "<h1>PIN2 - Terraform y AWS </h1><h2>Curso DevOps - MundosE </h2><h3>Grupo 14</h3>" > /var/www/html/index.html
+echo "<h1>MUNDOS E - PIN 2 - DevOps 2403</h1><h2> </h2><h3>Grupo 4</h3>" > /var/www/html/index.html
 systemctl restart apache2
