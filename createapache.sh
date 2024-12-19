0977a0d22ba1c8e54e1d8d0270edcd1fd984fec8
julianmrios <julianmrios@gmail.com>
2024-12-19 17:05:33 -0300

    1.0

--- createapache.sh
+++ createapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y apache2
-echo "<h1>DIPLOMATURA DEVOPS - COMISION 2403</h1><h2>MUNDOS E</h2><h2>PIN 2</h2><h3>Grupo 4</h3>" > /var/www/html/index.html
+echo "<h1>DIPLOMATURA DEVOPS - COMISION 2403</h1><h2>MUNDOS E</h2><h2>PIN 2</h2><h3>Grupo 7</h3>" > /var/www/html/index.html
 systemctl restart apache2
